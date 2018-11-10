e5ea94cfabf041edcb5e5880d75409edcec785e7
abhishekgpta <gptaabhishek93@gmail.com>
2018-11-10 13:10:03 +0530

    Setup Production WE pack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,52 @@
 //entry -> output
 const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = (env)=>{
+	const isProduction = env === 'production';
+	const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' }); 
+	// const cssExtract = new ExtractTextPlugin('style.css')
 
-module.exports={
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname,'public'),
-		filename: 'bundle.js'
-	},
-	module:{
-		rules:[{
-			loader:'babel-loader',
-			test: /\.js$/,
-			exclude:/node_modules/
-		},{
-			use:['style-loader', 'css-loader','sass-loader'],
-			test: /\.s?css$/,
-		}]
-	},
-	devtool:'cheap-module-eval-source-map',
-	devServer:{
-		contentBase: path.join(__dirname,'public'),
-		historyApiFallback:true
-	}
+	return{
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname,'public'),
+			filename: 'bundle.js'
+		},
+		module:{
+			rules:[{
+				loader:'babel-loader',
+				test: /\.js$/,
+				exclude:/node_modules/
+			},{
+				test: /\.s?css$/,
+				// use: CSSExtract.extract({
+				// 	use:['css-loader','sass-loader'],
+				// })
+				use: [
+				     MiniCssExtractPlugin.loader,
+				     {
+				         loader: 'css-loader',
+				         options: {
+				             sourceMap: true
+				         }
+				     },
+				     {
+				         loader: 'sass-loader',
+				         options: {
+				             sourceMap: true
+				         }
+				     }
+				 ]
+			}]
+		},
+		plugins:[
+			CSSExtract
+		],
+		devtool: isProduction?'source-map': 'inline-source-map',
+		devServer:{
+			contentBase: path.join(__dirname,'public'),
+			historyApiFallback:true
+		}
+	};
 };
